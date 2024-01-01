ca71816d2472c65626c5fbecfc8d716be44cd617
updaun <updauney@daum.net>
2024-01-01 16:12:39 +0900

    feat: add elastic search for Article model

---
+++ core_apps/search/serializers.py
@@ -0,0 +1,15 @@
+from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
+from .documents import ArticleDocument
+
+
+class ArticleElasticSearchSerializer(DocumentSerializer):
+    class Meta(object):
+        document = ArticleDocument
+        fields = (
+            "title",
+            "author",
+            "slug",
+            "description",
+            "body",
+            "created_at",
+        )
